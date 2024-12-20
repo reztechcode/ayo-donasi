37eeea7f9e42999d77d120d4b0713c441166191a
Irez Abdullah <irezabdullah@rezweb.my.id>
2024-12-20 18:11:41 +0700

    db-transaction: menambahkan kolom response text

--- database/migrations/2024_12_18_100032_create_transactions_table.php
+++ database/migrations/2024_12_18_100032_create_transactions_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('midtrans_order_id')->unique();
             $table->unsignedBigInteger('gross_amount');
             $table->enum('status',['pending','failed','completed'])->default('pending');
+            $table->string('response_text')->nullable();
             $table->foreign('donation_id')->references('donation_id')->on('donations')->onDelete('cascade');
             $table->timestamps();
         });
