c27e012d1ad9880580f72ff19a8a5946e5321845
Irez Abdullah <irezabdullah@rezweb.my.id>
2024-12-20 09:01:55 +0700

    refactor: perubahan penmaan routes untuk sisi user

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,18 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/Selengkapnya', [DonasiController::class, 'index'] );
-Route::get('/Detail', [DonasiController::class, 'DetailDonasi'] );
-Route::get('/Tentangkami', [DonasiController::class, 'TentangKami'] );
-Route::get('/Category', [DonasiController::class, 'CategoryDonasi'] );
-
-Route::middleware(['guest'])->group(function () {
-    Route::get('auth/login', [AuthController::class, 'showLogin'])->name('login');
-    Route::post('auth/login', [AuthController::class, 'authenticate']);
-});
+Route::get('/selengakapnya', [DonasiController::class, 'index'] );
+Route::get('/detail', [DonasiController::class, 'DetailDonasi'] );
+Route::get('/tentang-kami', [DonasiController::class, 'TentangKami'] );
+Route::get('/category', [DonasiController::class, 'CategoryDonasi'] );
 
 Route::prefix('admin')->middleware(['admin'])->group(function () {
     Route::resource('userman', UsermanController::class);
     Route::resource('categories', CategoryController::class);
     Route::resource('campaigns', CampaignController::class);
+});
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('auth/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('auth/login', [AuthController::class, 'authenticate']);
 });
